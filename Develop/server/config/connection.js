2bd84bab87ef65392e8ec7443542b0eae1ad2834
Owen Taylor <Owentaylor60@yahoo.com>
2022-07-29 13:01:33 -0400

    heroku fix

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config('../config.env');
+console.log(process.env.MONGODB_URI)
 MONGO_URL = process.env.MONGODB_URI ||'mongodb://localhost/googlebooks'
+console.log(MONGO_URL)
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
